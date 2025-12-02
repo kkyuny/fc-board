e3b4f60ab4ad9d861c25d43c6359456d0003124b
kyun <kyun9911@gmail.com>
2025-12-02 14:31:33 +0900

    Design API, DOMAIN for like

--- src/main/kotlin/com/fastcampus/fcboard/controller/dto/PostSummaryResponse.kt
+++ src/main/kotlin/com/fastcampus/fcboard/controller/dto/PostSummaryResponse.kt
@@ -10,7 +10,8 @@ data class PostSummaryResponse(
     val title: String,
     val createdBy: String,
     val createdAt: String,
-    val tag: String? = null
+    val tag: String? = null,
+    val likeCount: Long = 0,
 )
 
 fun Page<PostSummaryResponseDto>.toResponse() = PageImpl(
@@ -23,5 +24,6 @@ fun PostSummaryResponseDto.toResponse() = PostSummaryResponse(
     id = id,
     title = title,
     createdBy = createdBy,
-    createdAt = createdAt
+    createdAt = createdAt,
+    tag = firstTag
 )
