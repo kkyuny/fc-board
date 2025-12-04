b5f9519ce1ee19c857c67f96a4434e248dcf284f
kyun <kyun9911@gmail.com>
2025-12-04 14:25:59 +0900

    Config redis

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,10 +34,11 @@ dependencies {
     runtimeOnly("com.mysql:mysql-connector-j")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("com.h2database:h2")
     testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
     testImplementation("io.kotest:kotest-assertions-core:5.9.1")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.3.0")
-    testImplementation("com.h2database:h2")
+    testImplementation("io.kotest.extensions:kotest-extensions-testcontainers:2.0.2")
 }
 
 kotlin {
